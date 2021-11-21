dab112944dc84eeae81053403f183cf1e838f0ce
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2021-11-21 14:38:32 +0200

    Cleanup Tile class

--- src/main/java/com/example/game2048/Game/Tile.java
+++ src/main/java/com/example/game2048/Game/Tile.java
@@ -17,25 +17,11 @@ public class Tile {
         this.merged = true;
     }
 
-    public boolean isMerged() {
-        return this.merged;
-    }
-
     public void clearMerged() {
         this.merged = false;
     }
 
     public boolean isMergeableWith(Tile other) {
         return !this.merged && this.value == other.value;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj.getClass() != this.getClass()) return false;
-
-        Tile otherTile = (Tile) obj;
-
-        return this.value == otherTile.value && this.merged == otherTile.merged;
     }
 }
